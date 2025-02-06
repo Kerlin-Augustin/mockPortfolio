da88d88655441fa9ce6ea8bb729745a534c4d355
Kerlin Augustin <kerlin.augustin1@gmail.com>
2025-02-06 16:26:34 -0500

    added content that should be in the portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src="image.png" width={300}/>
+        <h2>Kerlin Augustin</h2>
+        <h3>Contact Info</h3>
+        <p>Enter Email Address</p>
+        <h3>Community Building</h3>
+        <p>LinkedIn</p>
+        <p>Twitter</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <h2>About Me</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe quaerat nihil quis nostrum incidunt cupiditate aliquam, quam quasi, suscipit distinctio quae voluptates eius. Modi itaque similique accusamus aliquid dolorem fugiat voluptas autem, veniam rem voluptatum ipsam maiores harum architecto fugit? Incidunt veniam atque nisi voluptas. Accusantium exercitationem repudiandae, quis quaerat dicta assumenda deleniti! Saepe voluptatem ad dolorem perferendis repudiandae quod reiciendis doloribus aperiam maxime nobis. Cupiditate consequuntur recusandae placeat doloribus, assumenda a similique vitae odio quae. Quisquam, assumenda ab. Ab earum iure cum omnis non adipisci dolores libero vitae facere, error asperiores, quae numquam tempore nisi sapiente temporibus sunt natus!</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h2>Projects</h2>
+        <div>GoSavant</div>
+        <div>Talent Bridge</div>
+      </div>
     </>
   )
 }
